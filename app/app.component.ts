92621348275ac4346b8f9a2fe700d30ca69e1e37
Joe Eames <joeeames@gmail.com>
2016-06-15 10:28:50 -0600

    added nav

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { EventsListComponent } from './events/events-list.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @Component({
   selector: 'events-app',
   template: `
+    <nav-bar></nav-bar>
     <events-list></events-list>
   `,
   directives: [
-    EventsListComponent
+    EventsListComponent,
+    NavBarComponent
   ]
 })
 export class AppComponent {
---
+++ app/nav/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'nav-bar',
+  templateUrl: 'navbar.component.html'
+})
+export class NavBarComponent {
+  constructor() { }
+
+
+}
