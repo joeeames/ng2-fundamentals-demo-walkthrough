cd0a668f1faa49c88a871c3c31dc8f2395082656
joeeames <joeeames@gmail.com>
2016-06-16 16:05:28 -0600

    moved profile to model based

--- app/users/profile.component.ts
+++ app/users/profile.component.ts
@@ -1,19 +1,36 @@
 import { Component } from '@angular/core';
+import { FORM_DIRECTIVES, FormBuilder, Control, ControlGroup, Validators } from '@angular/common';
 import { Router } from '@angular/router-deprecated';
 import { AuthService } from './auth.service';
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
   selector: 'user-profile',
   templateUrl: 'app/users/profile.component.html',
 })
 export class ProfileComponent {
-  
-  constructor(private router: Router,
-    private auth: AuthService) {
+  firstName: Control;
+  lastName: Control;
+  profileForm: ControlGroup;
+    
+  constructor(private auth : AuthService,
+    private builder: FormBuilder,
+      private router: Router,
+      private toastr: ToastrService) {
+    this.firstName = new Control(this.auth.currentUser.firstName, Validators.required);
+    this.lastName = new Control(this.auth.currentUser.lastName, Validators.required);
+
+    this.profileForm = builder.group({
+      firstName: this.firstName,
+      lastName: this.lastName,
+    })
   }
   
   saveProfile(formValues) {
-    this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
+    if(this.profileForm.valid) {
+      this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.toastr.success('Profile Saved');
+    }
   }
   
   cancel() {
