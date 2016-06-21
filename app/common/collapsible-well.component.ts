21e643f7a233d4675bd19dedf3b1ad87e0663490
Joe Eames <joeeames@gmail.com>
2016-06-20 19:40:35 -0600

    added collapsible well

---
+++ app/common/collapsible-well.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'collapsible-well',
+  template:`
+<div (click)="toggleContent()" class="well pointable">
+  <h4 class="well-title">{{title}}</h4>
+  <ng-content *ngIf="visible"></ng-content>
+</div>
+  `,
+})
+export class CollapsibleWellComponent {
+  @Input() title: string;
+  visible: boolean = true;
+  
+  toggleContent() {
+    console.log('toggling', this.visible);
+    this.visible = !this.visible;
+  }
+}
+
--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Session } from '../shared/index';
 import { AuthService } from '../../users/auth.service';
+import { CollapsibleWellComponent } from '../../common/collapsible-well.component';
 
 
 @Component({
@@ -8,6 +9,7 @@ import { AuthService } from '../../users/auth.service';
   selector: 'session-list',
   templateUrl: 'session-list.component.html',
   styles: ['collapsible-well h6 {margin-top:-5px; margin-bottom:10px }'],
+  directives: [CollapsibleWellComponent]
 })
 export class SessionListComponent {
   @Input() sessions: Session[];
