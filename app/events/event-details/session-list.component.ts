aa1f83e5e66518cd0172bf7e14370f6d04dbec1b
Joe Eames <joeeames@gmail.com>
2016-06-20 19:55:11 -0600

    added the duration pipe

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Session } from '../shared/index';
+import { Session, DurationPipe } from '../shared/index';
 import { AuthService } from '../../users/auth.service';
 import { CollapsibleWellComponent } from '../../common/collapsible-well.component';
 
@@ -9,7 +9,8 @@ import { CollapsibleWellComponent } from '../../common/collapsible-well.componen
   selector: 'session-list',
   templateUrl: 'session-list.component.html',
   styles: ['collapsible-well h6 {margin-top:-5px; margin-bottom:10px }'],
-  directives: [CollapsibleWellComponent]
+  directives: [CollapsibleWellComponent],
+  pipes: [DurationPipe]
 })
 export class SessionListComponent {
   @Input() sessions: Session[];
