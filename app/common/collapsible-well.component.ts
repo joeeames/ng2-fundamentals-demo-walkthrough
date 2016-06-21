3a7944a4d0a02539669041564faefb221bfae128
Joe Eames <joeeames@gmail.com>
2016-06-20 19:44:17 -0600

    added a 2nd content projection slot to the collapsible well

--- app/common/collapsible-well.component.ts
+++ app/common/collapsible-well.component.ts
@@ -3,14 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'collapsible-well',
   template:`
-<div (click)="toggleContent()" class="well pointable">
-  <h4 class="well-title">{{title}}</h4>
-  <ng-content *ngIf="visible"></ng-content>
-</div>
+    <div (click)="toggleContent()" class="well pointable">
+      <h4>
+        <ng-content select="well-title"></ng-content>
+      </h4>
+      <ng-content select="well-body" *ngIf="visible"></ng-content>
+    </div>
   `,
 })
 export class CollapsibleWellComponent {
-  @Input() title: string;
   visible: boolean = true;
   
   toggleContent() {
