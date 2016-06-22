9e7e04d2d6ada1e0d4cda2c1573a5df597afee5e
Joe Eames <joeeames@gmail.com>
2016-06-22 11:58:54 -0600

    persisting votes on server

--- app/events/event-details/voter.service.ts
+++ app/events/event-details/voter.service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Event, Session } from '../shared/event.model'
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class VoterService {
   
-  constructor() {}
+  constructor(private http: Http) {}
 
   deleteVoter(eventId: number, session: Session, voterName: string) {
     session.voters = session.voters.filter(voter => voter !== voterName);
+
+    this.http.delete(`/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`)
+      .catch(this.handleError)
+      .subscribe()
   }
 
   addVoter(eventId: number, session: Session, voterName: string) {
     session.voters.push(voterName); 
+
+    let headers = new Headers({ 'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers });
+    
+    var url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+    this.http.post(url, JSON.stringify({}), options)
+      .catch(this.handleError)
+      .subscribe()
   }
 
   userHasVoted(session: Session, userName: string) {
     return session.voters.some(voter => voter === userName)
+  }
+
+  private handleError(error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || "Server Error");
   }
 }
